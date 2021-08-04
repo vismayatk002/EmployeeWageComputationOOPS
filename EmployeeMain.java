9131a31750fbfb8ed8dc33cffbf2bf58e0f13cba
vismayatk002 <vismayatk002@gmail.com>
2021-08-04 16:26:18 +0530

    Refactor : calculated part time Daily employee wage

--- EmployeeMain.java
+++ EmployeeMain.java
@@ -3,8 +3,9 @@ public class EmployeeMain{
     public static void main(String[] args){
         EmployeeWage emp = new EmployeeWage();
         double empCheck = Math.floor (Math.random() * 10) % 2;
+        double empType = Math.floor (Math.random() * 10) % 2;
         if(emp.checkIsPresent(empCheck)){
-            emp.calcDailyEmpWage();
+            emp.calcDailyEmpWage(empType);
         }
         else{
             System.out.println("Employee is absent");
--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,9 @@
 class EmployeeWage{
       
     int IS_PRESENT = 1;
-    int WAGE_PER_HR=20;
-    int FULLDAY_HR=8;
-
+    int WAGE_PER_HR = 20;
+    int FULLDAY_HR = 16;
+    int FULLTIME = 1;
     public boolean checkIsPresent(double empCheck){
         if(empCheck == IS_PRESENT){
             return true;
@@ -12,8 +12,15 @@ class EmployeeWage{
             return false;
         }
     }
-    public void calcDailyEmpWage(){
+    public void calcDailyEmpWage(double empType){
         int dailyEmpWage =  WAGE_PER_HR * FULLDAY_HR;
-        System.out.println("Daily Employee Wage :" + dailyEmpWage);
+        if(empType == FULLTIME){
+            System.out.println("Employee Type : Full time");
+            System.out.println("Daily Employee Wage :" + dailyEmpWage);
+        }
+        else{
+            System.out.println("Employee Type : Part time");
+            System.out.println("Daily Employee Wage :" + (dailyEmpWage / 2));
+        }
     }
 }
