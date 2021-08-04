8dbd7d15fb0f92dfbda4f22162da5bb110746d8f
vismayatk002 <vismayatk002@gmail.com>
2021-08-04 17:22:15 +0530

    Add : swich case

--- EmployeeMain.java
+++ EmployeeMain.java
@@ -2,8 +2,8 @@ public class EmployeeMain{
 
     public static void main(String[] args){
         EmployeeWage emp = new EmployeeWage();
-        double empCheck = Math.floor (Math.random() * 10) % 2;
-        double empType = Math.floor (Math.random() * 10) % 2;
+        int empCheck = (int)(Math.random() * 10) % 2;
+        int empType = (int)(Math.random() * 10) % 2;
         if(emp.checkIsPresent(empCheck)){
             emp.calcDailyEmpWage(empType);
         }
--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,26 +1,27 @@
 class EmployeeWage{
       
-    int IS_PRESENT = 1;
-    int WAGE_PER_HR = 20;
-    int FULLDAY_HR = 16;
-    int FULLTIME = 1;
-    public boolean checkIsPresent(double empCheck){
-        if(empCheck == IS_PRESENT){
+    final int IS_PRESENT = 1;
+    final int FULL_TIME = 1;
+    int wagePerHr = 20;
+    int fullDayHr= 16;
+    public boolean checkIsPresent(int empCheck){
+        if(empCheck == IS_PRESENT){ 
             return true;
         }
-        else{
+        else{ 
             return false;
         }
     }
-    public void calcDailyEmpWage(double empType){
-        int dailyEmpWage =  WAGE_PER_HR * FULLDAY_HR;
-        if(empType == FULLTIME){
-            System.out.println("Employee Type : Full time");
-            System.out.println("Daily Employee Wage :" + dailyEmpWage);
-        }
-        else{
-            System.out.println("Employee Type : Part time");
-            System.out.println("Daily Employee Wage :" + (dailyEmpWage / 2));
+    public void calcDailyEmpWage(int empType){
+        int dailyEmpWage =  wagePerHr * wagePerHr;
+        switch(empType){
+            case  FULL_TIME : 
+                System.out.println("Employee Type : Full time");
+                System.out.println("Daily Employee Wage :" + dailyEmpWage);
+            break;
+            default : 
+                System.out.println("Employee Type : Part time");
+                System.out.println("Daily Employee Wage :" + (dailyEmpWage / 2));
         }
     }
 }
